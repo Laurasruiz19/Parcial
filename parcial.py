778b177455ba3fa262779bcdf7feea648b5d46d7
laurals30 <lsruiz96337@umanizales.edu.co>
2022-03-29 20:13:18 -0600

    updated

--- parcial.py
+++ parcial.py
@@ -1,1 +1,7 @@
 #Dado S, Determinar el número de palabras en S
+
+def conversion_case(nombre_variable):
+    return ''.join(p.capitalize() for p in nombre_variable.split('_'))
+
+    print(conversion_case('nombre_variable'))
+    print(conversion_case('palabra_min'))
